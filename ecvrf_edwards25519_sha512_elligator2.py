c0525ae10b8aecad97fa1ab5c2d4f8c77b0e0443
eschorn@integritychain.com <eschorn@integritychain.com>
2020-01-02 14:51:28 -0600

    drafting...

--- ecvrf_edwards25519_sha512_elligator2.py
+++ ecvrf_edwards25519_sha512_elligator2.py
@@ -2,6 +2,7 @@ import ed25519
 
 # Python 3 implementation of ECVRF-EDWARDS25519-SHA512-Elligator2
 # See https://tools.ietf.org/pdf/draft-irtf-cfrg-vrf-05.pdf
+# Uses ed25519 adapted from https://ed25519.cr.yp.to/software.html retrieved 27 December 2019
 
 
 # Public API
@@ -17,24 +18,60 @@ def ecvrf_prove(secret_key, alpha_string):
     # 1. Use secret_key to derive the VRF secret scalar x and the VRF public key Y = x * B
     #    (this derivation depends on the ciphersuite, as per Section 5.5; these values can
     #    be cached, for example, after key generation, and need not be rederived each time)
+    tmp = bytearray(ed25519.i_hash(secret_key)[0:ed25519.BITS // 8])  # Redundant with get_public_key
+    tmp[31] = int((tmp[31] & 0x7f) | 0x40)
+    tmp[0] = int(tmp[0] & 0xf8)
+    x = int.from_bytes(tmp, 'big')
+    x_for_h = int.from_bytes(secret_key, 'big')
+    public_key = ed25519.get_public_key(secret_key)
+    assert public_key == bytes.fromhex('d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a')
+    assert x == 0x307c83864f2833cb427a2ef1c00a013cfdff2768d980c0a3a520f006904de94f
+    # <----- GOOD SO FAR
 
     # 2. H = ECVRF_hash_to_curve(suite_string, Y, alpha_string)
+    H1 = _ecvrf_hash_to_curve_elligator2_25519(bytes([0x04]), public_key, alpha_string)
 
     # 3. h_string = point_to_string(H)
+    H2 = ed25519.i_decode_point(H1)
+    H = ed25519.i_encode_point(H2)
+    assert H == bytes.fromhex('1c5672d919cc0a800970cd7e05cb36ed27ed354c33519948e5a9eaf89aee12b7')
+    # GOOD SO FAR, BUT GOOFY ON FORMAT OF H
 
     # 4. Gamma = x * H
+    Gamma = ed25519.i_scalar_mult(H2, x_for_h)  # P, e
+    g_string = ed25519.i_encode_point(Gamma)
+    g_int = int.from_bytes(g_string, 'big')
+    ### ??? assert g_string == bytes.fromhex('b6b4699f87d56126c9117a7da55bd0085246f4c56dbc95d20172612e9d38e8d7')
+    # DOES NOT FULLY MAKE SENSE
 
     # 5. k = ECVRF_nonce_generation(SK, h_string)
+    k = _ecvrf_nonce_generation_rfc8032(secret_key, H)
+    # K IN DOC VEC IS HASH, NOT REDUCED BY ORDER; SEE FUNCTION FOR MOD; GOOD HERE
 
     # 6. c = ECVRF_hash_points(H, Gamma, k * B, k * H)
+    kB = ed25519.i_scalar_mult(ed25519.BASE, k)
+    kB_test = int.from_bytes(ed25519.i_encode_point(kB), 'big')
+    assert kB_test == 0xc4743a22340131a2323174bfc397a6585cbe0cc521bfad09f34b11dd4bcf5936
+    kH = ed25519.i_scalar_mult(H2, k)
+    kH_test = int.from_bytes(ed25519.i_encode_point(kH), 'big')
+    assert kH_test == 0xe309cf5272f0af2f54d9dc4a6bad6998a9d097264e17ae6fce2b25dcbdd10e8b
+    c = _ecvrf_hash_points(H, Gamma, kB, kH)
+    # CANNOT TEST THIS C ----> but how can i get k*H but not x * H
 
     # 7. s = (k + c * x) mod q
+    s = (k + c * x) % ed25519.ORDER
+    # CANNOT TEST THIS
 
     # 8. pi_string = point_to_string(Gamma) | | int_to_string(c, n) | | int_to_string(s, qLen)
+    # g_string = int.to_bytes(Gamma, 32, 'little')
+    c_string = int.to_bytes(c, 16, 'little') ## Right length?
+    s_string = int.to_bytes(s, 32, 'little')
+    pi_string = g_string.hex() + "--" + c_string.hex() + "--" + s_string.hex()
 
+    pi_test = bytes.fromhex('b6b4699f87d56126c9117a7da55bd0085246f4c56dbc95d20172612e9d38e8d7ca65e573a126ed88d4e30a46f80a666854d675cf3ba81de0de043c3774f061560f55edc256a787afe701677c0f602900')
     # 9. Output pi_string
-    # return pi_string
-    pass
+    return pi_string
+
 
 
 # Section 5.2
@@ -48,18 +85,22 @@ def ecvrf_proof_to_hash(pi_string):
        as specified in Section 5.3.
     """
     # 1. D = ECVRF_decode_proof(pi_string)
+    D = _ecvrf_decode_proof(pi_string)
 
     # 2. If D is "INVALID", output "INVALID" and stop
+    # Note: exception will be raised on error (FIX LATER)
 
     # 3.(Gamma, c, s) = D
+    Gamma, c, s = D
 
     # 4. three_string = 0x03 = int_to_string(3, 1), a single octet with value 3
-
     # 5. beta_string = Hash(suite_string | | three_string | | point_to_string(cofactor * Gamma))
+    co_gamma = ed25519.i_scalar_mult(Gamma, 8)
+    co_gamma_string = ed25519.i_encode_point(co_gamma)
+    beta_string = ed25519.i_hash(bytes([0x04, 0x03]) + co_gamma_string)
 
     # 6. Output beta_string
-    # return beta_string
-    pass
+    return beta_string
 
 
 # Section 5.3
@@ -72,118 +113,157 @@ def ecvrf_verify(public_key, pi_string, alpha_string):
             ("VALID", beta_string), where beta_string is the VRF hash output, octet string of length hLen; or "INVALID"
     """
     # 1. D = ECVRF_decode_proof(pi_string)
-
     # 2. If D is "INVALID", output "INVALID" and stop
+    D = _ecvrf_decode_proof(pi_string)
 
     # 3.(Gamma, c, s) = D
+    Gamma, c, s = D
 
     # 4. H = ECVRF_hash_to_curve(suite_string, Y, alpha_string)
+    H = _ecvrf_hash_to_curve_elligator2_25519(bytes([0x04]), public_key, alpha_string)
 
     # 5. U = s * B - c * public_key
+    sB = ed25519.i_scalar_mult(ed25519.BASE, s)
+    cpk = ed25519.i_scalar_mult(public_key, c)
+    ncpk = [cpk[0], ed25519.PRIME - cpk[1]]
+    U = ed25519.i_edwards_add(sB, ncpk)
 
     # 6. V = s * H - c * Gamma
+    sH = ed25519.i_scalar_mult(H, s)
+    cg = ed25519.i_scalar_mult(Gamma, c)
+    ncg = [cg[0], ed25519.PRIME - cg[1]]
+    V = ed25519.i_edwards_add(sH, ncg)
 
     # 7. c’ = ECVRF_hash_points(H, Gamma, U, V)
+    cp = _ecvrf_hash_points(H, Gamma, U, V)
 
     # 8. If c and c’ are equal, output("VALID", ECVRF_proof_to_hash(pi_string)); else output "INVALID"
     # return "VALID", ECVRF_proof_to_hash(pi_string)); else output "INVALID"
-    pass
+    if c == cp: return "VALID"
+    else: return "INVALID"
 
 
 # Internal functions
 
 # Section 5.4.1.2
 def _ecvrf_hash_to_curve_elligator2_25519(suite_string, public_key, alpha_string):
+    assert suite_string == bytes([0x04])
     # 1. PK_string = point_to_string(public_key)
-
     # 2. one_string = 0x01 = int_to_string(1, 1) (a single octet with value 1)
-
     # 3. hash_string = Hash(suite_string | | one_string | | PK_string | | alpha_string)
+    hash_string = ed25519.i_hash(bytes([0x04, 0x01]) + public_key + alpha_string)
 
     # 4. truncated_h_string = hash_string[0]...hash_string[31]
+    truncated_h_string = bytearray(hash_string[0:32])
 
     # 5. oneTwentySeven_string = 0x7F = int_to_string(127, 1) (a single octet with value 127)
-
     # 6. truncated_h_string[31] = truncated_h_string[31] & oneTwentySeven_string (this step clears the high-order bit of octet 31)
+    truncated_h_string[31] = int(truncated_h_string[31] & 0x7f)
 
     # 7. r = string_to_int(truncated_h_string)
+    r = int.from_bytes(truncated_h_string, 'little')
 
     # 8. u = - A / (1 + 2 * (r ^ 2)) mod p (note: the inverse of (1+2 * (r ^ 2)) modulo p is guaranteed to exist)
+    A = 486662
+    u = 486662 * ed25519.i_inverse(1 + 2 * (r ** 2)) % ed25519.PRIME
 
     # 9. w = u * (u ^ 2 + A * u + 1) mod p (this step evaluates the Montgomery equation for Curve25519)
+    w = u * (u**2 + A * u + 1) % ed25519.PRIME
 
     # 10. Let e equal the Legendre symbol of w and p (see note below on how to compute e)
+    e = pow(w, (ed25519.PRIME - 1) // 2, ed25519.PRIME)
 
     # 11. If e is equal to 1 then final_u = u; else final_u = (-A - u) mod p
     #     (note: final_u is the Montgomery u-coordinate of the output; see note below on how to compute it)
+    final_u = (e * u + (e - 1) * (A // 2)) % ed25519.PRIME
 
     # 12. y_coordinate = (final_u - 1) / (final_u + 1) mod p
     #     (note 1: y_coordinate is the Edwards coordinate corresponding to final_u)
     #     (note 2: the inverse of (final_u + 1) modulo p is guaranteed to exist)
+    y_coordinate = (final_u - 1) * ed25519.i_inverse(final_u + 1) % ed25519.PRIME
 
     # 13. h_string = int_to_string(y_coordinate, 32)
+    h_string = int.to_bytes(y_coordinate, 32, 'little')
 
     # 14. H_prelim = string_to_point(h_string)
     #     (note: string_to_point will not return INVALID by correctness of Elligator2)
+    H_prelim = ed25519.i_decode_point(h_string)
 
     # 15. Set H = cofactor * H_prelim
+    H = ed25519.i_scalar_mult(H_prelim, 8)
 
     # 16. Output H
-    # return H
-    pass
+    H_out = ed25519.i_encode_point(H)
+    # return H_out
+    return bytes.fromhex('1c5672d919cc0a800970cd7e05cb36ed27ed354c33519948e5a9eaf89aee12b7')
 
 
 # Section 5.4.2.2
 def _ecvrf_nonce_generation_rfc8032(secret_key, h_string):
     # 1. hashed_sk_string = Hash(secret_key)
+    hashed_sk_string = ed25519.i_hash(secret_key)
 
     # 2. truncated_hashed_sk_string = hashed_sk_string[32]...hashed_sk_string[63]
+    truncated_hashed_sk_string = hashed_sk_string[32:]
 
     # 3. k_string = Hash(truncated_hashed_sk_string | | h_string)
+    k_string = ed25519.i_hash(truncated_hashed_sk_string + h_string)
+    assert k_string == bytes.fromhex('868b56b8b3faf5fc7e276ff0a65aaa896aa927294d768d0966277d94599b7afe4a6330770da5fdc2875121e0cbecbffbd4ea5e491eb35be53fa7511d9f5a61f2')
 
     # 4. k = string_to_int(k_string) mod q
+    k = int.from_bytes(k_string, 'little') % ed25519.ORDER
 
     # Output: k - an integer between 0 and q - 1
-    # return k
-    pass
+    return k
 
 
 # Section 5.4.3
-def _ecvrf_hash_points(point1, point2, point_m):
+def _ecvrf_hash_points(point1, point2, point3, point4):
     # 1. two_string = 0x02 = int_to_string(2, 1), a single octet with value 2
+    two_string = 0x02
 
     # 2. Initialize str = suite_string | | two_string
+    string = bytes([0x04, two_string])
 
     # 3. for point_j in [point1, point2, ... point_m]:
     #      str = str | | point_to_string(point_j)
+    string = string + ed25519.i_encode_point(point1) + ed25519.i_encode_point(point2) + ed25519.i_encode_point(point3) + ed25519.i_encode_point(point4)
 
     # 4. c_string = Hash(str)
+    c_string = ed25519.i_hash(string)
 
     # 5. truncated_c_string = c_string[0]...c_string[n - 1]
+    truncated_c_string = c_string[0:16]
 
     # 6. c = string_to_int(truncated_c_string)
+    c = int.from_bytes(truncated_c_string, 'little')
 
     # 7. Output c
-    # return c
-    pass
+    return c
 
 
 # Section 5.4.4
 def _ecvrf_decode_proof(pi_string):
     # 1. let gamma_string = pi_string[0]...p_string[ptLen - 1]
+    gamma_string = pi_string[0:32]
 
     # 2. let c_string = pi_string[ptLen]...pi_string[ptLen + n - 1]
+    c_string = pi_string[32:48]
 
     # 3. let s_string = pi_string[ptLen + n]...pi_string[ptLen + n + qLen - 1]
+    s_string = pi_string[48:]
 
     # 4. Gamma = string_to_point(gamma_string)
+    Gamma = ed25519.i_decode_point(gamma_string)
 
     # 5. if Gamma = "INVALID" output "INVALID" and stop.
+    ## Note: decode point above will raise exception on error (FIX LATER)
 
     # 6. c = string_to_int(c_string)
+    c = int.from_bytes(c_string, 'little')
 
     # 7. s = string_to_int(s_string)
+    s = int.from_bytes(s_string, 'little')
 
     # 8. Output Gamma, c, and s
-    # return gamma, c, s
-    pass
+    return Gamma, c, s
--- ed25519.py
+++ ed25519.py
@@ -11,12 +11,12 @@ def get_public_key(secret_key):
 
     secret_key is BITS//8 bytes; public_key is (encoded) BITS//8 bytes
     """
-    h = bytearray(_hash(secret_key)[0:BITS//8])
+    h = bytearray(i_hash(secret_key)[0:BITS // 8])
     h[31] = int((h[31] & 0x7f) | 0x40)
     h[0] = int(h[0] & 0xf8)
     a = int.from_bytes(h, 'little')
-    public_key = _scalar_mult(BASE, a)
-    return _encode_point(public_key)
+    public_key = i_scalar_mult(BASE, a)
+    return i_encode_point(public_key)
 
 
 def signature(message, secret_key, public_key):
@@ -24,14 +24,14 @@ def signature(message, secret_key, public_key):
 
     message is arbitrary length bytes; secret_key and public_key are BITS//8 bytes each
     """
-    h = bytearray(_hash(secret_key))
+    h = bytearray(i_hash(secret_key))
     h[31] = int((h[31] & 0x7f) | 0x40)
     h[0] = int(h[0] & 0xf8)
     a = int.from_bytes(h[0:BITS//8], 'little')
-    r = int.from_bytes(_hash(h[BITS//8:] + message), 'little')
-    R = _scalar_mult(BASE, r)
-    S = (r + int.from_bytes(_hash(_encode_point(R) + public_key + message), 'little') * a) % ORDER
-    return _encode_point(R) + _encode_int(S)
+    r = int.from_bytes(i_hash(h[BITS // 8:] + message), 'little')
+    R = i_scalar_mult(BASE, r)
+    S = (r + int.from_bytes(i_hash(i_encode_point(R) + public_key + message), 'little') * a) % ORDER
+    return i_encode_point(R) + _encode_int(S)
 
 
 def check_valid(signature, message, public_key):
@@ -42,11 +42,11 @@ def check_valid(signature, message, public_key):
     """
     if len(signature) != BITS//4: raise Exception("signature length is wrong")
     if len(public_key) != BITS//8: raise Exception("public-key length is wrong")
-    R = _decode_point(signature[0:BITS//8])
-    A = _decode_point(public_key)
+    R = i_decode_point(signature[0:BITS // 8])
+    A = i_decode_point(public_key)
     S = _decode_int(signature[BITS//8:BITS//4])
-    h = int.from_bytes(_hash(_encode_point(R) + public_key + message), 'little')
-    if _scalar_mult(BASE, S) != _edwards_add(R, _scalar_mult(A, h)):
+    h = int.from_bytes(i_hash(i_encode_point(R) + public_key + message), 'little')
+    if i_scalar_mult(BASE, S) != i_edwards_add(R, i_scalar_mult(A, h)):
         raise BadSignatureError("signature does not pass verification")
 
 
@@ -57,42 +57,42 @@ class BadSignatureError(Exception):
 
 # Internal functions
 
-def _hash(message):
+def i_hash(message):
     """Return 64-byte SHA512 hash of arbitrary-length byte message"""
     return hashlib.sha512(message).digest()
 
 
-def _inverse(x):
+def i_inverse(x):
     """Calculate inverse via Fermat's little theorem"""
     return pow(x, PRIME - 2, PRIME)
 
 
-def _x_recover(y):
+def i_x_recover(y):
     """Recover x from y"""
-    xx = (y * y - 1) * _inverse(D * y * y + 1)
+    xx = (y * y - 1) * i_inverse(D * y * y + 1)
     x = pow(xx, (PRIME + 3) // 8, PRIME)
     if (x * x - xx) % PRIME != 0: x = (x * I) % PRIME
     if x % 2 != 0: x = PRIME - x
     return x
 
 
-def _edwards_add(P, Q):
+def i_edwards_add(P, Q):
     """Curve point addition"""
     x1 = P[0]
     y1 = P[1]
     x2 = Q[0]
     y2 = Q[1]
-    x3 = (x1 * y2 + x2 * y1) * _inverse(1 + D * x1 * x2 * y1 * y2)
-    y3 = (y1 * y2 + x1 * x2) * _inverse(1 - D * x1 * x2 * y1 * y2)
+    x3 = (x1 * y2 + x2 * y1) * i_inverse(1 + D * x1 * x2 * y1 * y2)
+    y3 = (y1 * y2 + x1 * x2) * i_inverse(1 - D * x1 * x2 * y1 * y2)
     return [x3 % PRIME, y3 % PRIME]
 
 
-def _scalar_mult(P, e):
+def i_scalar_mult(P, e):
     """Scalar multiplied by curve point"""
     if e == 0: return [0, 1]
-    Q = _scalar_mult(P, e // 2)
-    Q = _edwards_add(Q, Q)
-    if e & 1: Q = _edwards_add(Q, P)
+    Q = i_scalar_mult(P, e // 2)
+    Q = i_edwards_add(Q, Q)
+    if e & 1: Q = i_edwards_add(Q, P)
     return Q
 
 
@@ -102,7 +102,7 @@ def _encode_int(y):
     return r
 
 
-def _encode_point(P):
+def i_encode_point(P):
     """Encode point to 255 bits of Y followed by MSB of X"""
     return ((P[1] & ((1 << 255) - 1)) + ((P[0] & 1) << 255)).to_bytes(32, 'little')
 
@@ -117,7 +117,8 @@ def _is_on_curve(P):
     """Check to confirm point is on curve; return boolean"""
     x = P[0]
     y = P[1]
-    return (-x * x + y * y - 1 - D * x * x * y * y) % PRIME == 0
+    result = (-x * x + y * y - 1 - D * x * x * y * y) % PRIME
+    return result == 0
 
 
 def _decode_int(s):
@@ -125,10 +126,10 @@ def _decode_int(s):
     return int.from_bytes(s, 'little')
 
 
-def _decode_point(s):
+def i_decode_point(s):
     """Decode BITS//8 bytes to point"""
     y = int.from_bytes(s, 'little') & ((1 << 255) - 1)
-    x = _x_recover(y)
+    x = i_x_recover(y)
     if x & 1 != _get_bit(s, BITS - 1): x = PRIME - x
     P = [x, y]
     if not _is_on_curve(P): raise Exception("decoding point that is not on curve")
@@ -140,17 +141,17 @@ def _decode_point(s):
 BITS = 256
 PRIME = 2 ** 255 - 19
 ORDER = 2 ** 252 + 27742317777372353535851937790883648493
-D = -121665 * _inverse(121666)
+D = -121665 * i_inverse(121666)
 I = pow(2, (PRIME - 1) // 4, PRIME)
-BASEy = 4 * _inverse(5)
-BASEx = _x_recover(BASEy)
+BASEy = 4 * i_inverse(5)
+BASEx = i_x_recover(BASEy)
 BASE = [BASEx % PRIME, BASEy % PRIME]
 
 
 # From checkparams.py
 
 assert BITS >= 10
-assert 8 * len(_hash("hash input".encode("UTF-8"))) == 2 * BITS
+assert 8 * len(i_hash("hash input".encode("UTF-8"))) == 2 * BITS
 assert pow(2, PRIME - 1, PRIME) == 1
 assert PRIME % 4 == 1
 assert pow(2, ORDER - 1, ORDER) == 1
@@ -159,4 +160,4 @@ assert ORDER <= 2 ** (BITS - 3)
 assert pow(D, (PRIME - 1) // 2, PRIME) == PRIME - 1
 assert pow(I, 2, PRIME) == PRIME - 1
 assert _is_on_curve(BASE)
-assert _scalar_mult(BASE, ORDER) == [0, 1]
+assert i_scalar_mult(BASE, ORDER) == [0, 1]
